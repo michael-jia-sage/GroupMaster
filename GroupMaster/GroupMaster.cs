6bcbc3fee6b1efb6853b1615e8de0b194ef8fd0b
Michael Jia <Michael.Jia@sage.com>
2015-07-23 17:09:17 -0700

    created project

---
+++ GroupMaster/GroupMaster.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Parse;
+
+namespace GroupMaster
+{
+	public class App : Application
+	{
+		public App ()
+		{
+			// The root page of your application
+			MainPage = new ContentPage {
+				Content = new StackLayout {
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							XAlign = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}
+			};
+		}
+
+		protected override void OnStart ()
+		{
+			// Initialize the parse client with your Application ID and .NET Key found on
+			// your Parse dashboard
+			ParseClient.Initialize("evcq2mbbPHwCnCznl8yvbzvzMI7ij2G5G2v4oPcH",
+				"3Ms32KvfmlFhsfD7y0OBIrecP04N4KZJNPYfOZqb");
+			var testObject = new ParseObject ("MJTest");
+			testObject ["source"] = "android again";
+			testObject.SaveAsync ();
+		}
+
+		protected override void OnSleep ()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume ()
+		{
+			// Handle when your app resumes
+		}
+
+	}
+}
+
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Foundation;
+using UIKit;
+//using Parse;
+
+namespace GroupMaster.iOS
+{
+	[Register ("AppDelegate")]
+	public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+	{
+//		public AppDelegate ()
+//		{
+//			// Initialize the Parse client with your Application ID and .NET Key found on
+//			// your Parse dashboard
+//			ParseClient.Initialize("evcq2mbbPHwCnCznl8yvbzvzMI7ij2G5G2v4oPcH",
+//				"3Ms32KvfmlFhsfD7y0OBIrecP04N4KZJNPYfOZqb");
+//		}
+
+		public override bool FinishedLaunching (UIApplication app, NSDictionary options)
+		{
+			global::Xamarin.Forms.Forms.Init ();
+
+			// Code for starting up the Xamarin Test Cloud Agent
+			#if ENABLE_TEST_CLOUD
+			Xamarin.Calabash.Start();
+			#endif
+
+			LoadApplication (new App ());
+
+			return base.FinishedLaunching (app, options);
+		}
+	}
+}
+
